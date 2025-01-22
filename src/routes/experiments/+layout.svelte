5686803575da0db08f4acc4e616f92c78b576e2d
Jake Zohdi <jakez1@live.com>
2025-01-22 17:54:51 -0500

    oops

---
+++ src/routes/experiments/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import ExperimentsSideBar from '$lib/components/pages/experiments/ExperimentsSideBar.svelte';
+
+	let { children, data } = $props();
+
+	const experiments = data.experiments;
+</script>
+
+<ExperimentsSideBar {experiments} />
+{@render children?.()}
--- src/routes/experiments/conways-2d-webgpu/+layout.svelte
+++ src/routes/experiments/conways-2d-webgpu/+layout.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
 	import ErrorAnimation from '$lib/components/ErrorAnimation.svelte';
-	import ExperimentsSideBar from '$lib/components/pages/experiments/ExperimentsSideBar.svelte';
 
-	let { children, data } = $props();
+	let { children } = $props();
 	let error: { message: string } | null = $state(null);
 	let reset = $state(() => {});
 
-	const experiments = data.experiments;
 	function onerror(e: unknown, r: () => void) {
 		error = e as { message: string };
 		const message = error.message.split('\n')[0];
@@ -20,12 +18,5 @@
 	}
 </script>
 
-<ExperimentsSideBar {experiments} />
 <ErrorAnimation message={tryGetErrorMessage()} />
 <svelte:boundary {onerror}>{@render children?.()}</svelte:boundary>
-
-{#if error}
-	<ExperimentsSideBar {experiments} />
-
-	<div class=" content-center justify-items-center text-white">{tryGetErrorMessage()}</div>
-{/if}
