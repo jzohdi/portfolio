d422f4b2f2df896ac00604a16cf8b4fd2af969ef
jzohdi <31998568+jzohdi@users.noreply.github.com>
2025-03-02 20:13:31 -0500

    start 2048 exp

---
+++ src/routes/experiments/ai-2048/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import ExperimentLayout from '$lib/components/pages/experiments/ExperimentLayout.svelte';
+	import type { ParsedBlock } from '$lib/notion/server';
+	import type { Experiment } from '$lib/types/experiments';
+	import type { Snippet } from 'svelte';
+
+	let {
+		data,
+		children
+	}: { data: { blocks: ParsedBlock[]; experiment: Experiment }; children: Snippet } = $props();
+</script>
+
+<!-- <ExperimentLayout experiment={data.experiment} blocks={data.blocks}> -->
+{@render children?.()}
+<!-- </ExperimentLayout> -->
