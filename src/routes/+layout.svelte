ce649676da42987c15c8c53e95bbdfac51d328e4
Jake Zohdi <jakez1@live.com>
2025-06-07 14:20:30 +0200

    add analytics, update post, tim robinson jpegs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import { ModeWatcher } from 'mode-watcher';
 	import Layout from '$lib/components/Layout.svelte';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
+	import { dev } from '$app/environment';
 	import '../app.css';
+
 	let { children } = $props();
+
+	// Inject the Analytics functionality
+    injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <ModeWatcher />
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 import { saveImages } from './scripts/save-post-images';
 import dotenv from 'dotenv';
+import devtoolsJson from "vite-plugin-devtools-json";
 
 dotenv.config(); // load env vars from .env
 
 export default defineConfig({
-	plugins: [enhancedImages(), sveltekit(), saveImages()]
+	plugins: [enhancedImages(), sveltekit(), devtoolsJson(), saveImages()]
 });
